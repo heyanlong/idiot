e5bdd782a44f11b3b3513f5ce20cf5783ade766c
heyanlong <heyanlong@che001.com>
2018-10-10 10:43:38 +0800

    fix bug

--- src/Protocols/Dubbo.php
+++ src/Protocols/Dubbo.php
@@ -29,6 +29,7 @@ class Dubbo extends AbstractProtocol
 
     public function connect($host, $port, $path, $method, $args, $group, $version, $dubboVersion = self::DEFAULT_DUBBO_VERSION, $timeout = 10)
     {
+        $data = '';
         try
         {
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -37,7 +38,6 @@ class Dubbo extends AbstractProtocol
             $buffer = $this->buffer($path, $method, $args, $group, $version, $dubboVersion, $timeout * 1000);
             socket_write($socket, $buffer, strlen($buffer));
 
-            $data = '';
             $bl = 16;
             $read = [$socket];
             $write = [];
